de07af0945a31ca2ffaa8a7632cad9f056c02e3a
Kirill Bobin <75633909+krell11@users.noreply.github.com>
2024-04-30 22:49:05 +0300

    prompt fix

--- models/LLM.py
+++ models/LLM.py
@@ -2,51 +2,42 @@ from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 from torch import cuda
 import torch
-from llama_cpp import Llama
 import transformers
 import os
+from llama_cpp import Llama
 
 
 class LanguageModel:
+
     def __init__(self, model_name):
+        self.device = f'cuda:{cuda.current_device()}' \
+                      if cuda.is_available() else 'cpu'
 
-        self.device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
         self.model_name = model_name
+        self.temperature = 0.3
 
-        self.bnb_config = transformers.BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_type='nf4',
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_compute_dtype=torch.bfloat16
-        )
-        self.hf_token = os.environ.get("HUGGING_FACE_TOKEN")
-
-        # self.model_config = transformers.AutoConfig.from_pretrained(
-        #     model_name,
-        #     use_auth_token=self.hf_token
-        # )
-
-        self.model = transformers.AutoModelForCausalLM.from_pretrained(
-            self.model_name,
-            trust_remote=True,
-            #config=self.model_config,
-            quantization_config=self.bnb_config,
-            device_map='auto',
-            use_auth_token=self.hf_token
+        self.model = Llama(
+            model_path=model_name,
+            n_batch=2048,
+            n_gpu_layers=100,
+            temperature=self.temperature,
+            verbose=True
         )
 
-    def generate_text(self) -> transformers.pipeline:
-        generate_text = transformers.pipeline(
-            self.model_name,
-            tokenizer=transformers.AutoTokenizer.from_pretrained(self.model_name,use_auth_token=self.hf_token),
-            return_full_text=True,
-            task='text-generation',
-            temperature=0.0,
-            max_new_tokens=512,
-            repetition_penalty=1.1
-        )
-        return generate_text
+        self.prompt_template = """
+                        Ты - Бедолага, русскоязычный асистент, ты разговариаешь с людьми и помогаешь решить вопросы 
+                        связанные с работой банкаИспользуйте следующие триплеты знаний, чтобы ответить на вопрос 
+                        в конце. Если вы не знаете ответа, просто скажите, что не знаете, не пытайтесь придумать ответ.
+                        Старайся отвечать наиболее точно.
+                        
+                        {context}
+                        {links}
+                        """
 
-    def download_model(self):
-        self.model.eval()
-        print(f'Model_loaded on {self.device}')
+    def generate_text(self, user_prompt, max_tokens=100, top_p=0.1, echo=True, stop=("Q", "\n")):
+        output = self.model(user_prompt, top_p=top_p,
+                            max_tokens=max_tokens,
+                            temperature=self.temperature,
+                            echo=echo, stop=stop)
+
+        return output["choices"][0]["text"].strip()
