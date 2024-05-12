385a844e71f0d5966694cc6547b6ccbd63777e41
Kirill Bobin <75633909+krell11@users.noreply.github.com>
2024-05-12 03:56:39 +0300

    try of a model gpt2 based model for summarization

---
+++ models/summarizer.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+#херово работает вообще всегда
+class Summarizer:
+    def __init__(self, model_name: str = "IlyaGusev/rugpt3medium_sum_gazeta"):
+        self.model_name = model_name
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+
+    def generate_summarization(self, text: str):
+        text_tokens = self.tokenizer(
+                                    text,
+                                    max_length=20,
+                                    add_special_tokens=False,
+                                    padding=False,
+                                    truncation=True)["input_ids"]
+        input_ids = text_tokens + [self.tokenizer.sep_token_id]
+        input_ids = torch.LongTensor([input_ids])
+
+        output_ids = self.model.generate(
+            input_ids=input_ids,
+            no_repeat_ngram_size=4
+        )
+
+        summary = self.tokenizer.decode(output_ids[0], skip_special_tokens=False)
+        summary = summary.split(self.tokenizer.sep_token)[1]
+        summary = summary.split(self.tokenizer.eos_token)[0]
+        return summary
